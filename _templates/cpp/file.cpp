b21e5ca1e7b8dd957f64a3afe187b42c62874666
MaxMEllon <maxmellon1994@gmail.com>
2017-07-16 03:00:42 +0900

    Update

--- _templates/cpp/file.cpp
+++ _templates/cpp/file.cpp
@@ -1,4 +1,5 @@
 // {{{
+// {{{
 #include <cctype>
 #include <cerrno>
 #include <cfloat>
@@ -47,73 +48,54 @@
 #include <valarray>
 #include <vector>
 #include <array>
+// }}}
 
+// {{{
 #define int long long
 #define REP(i,n) for (int i=0;i<(n);i++)
 #define RREP(i,n) for (int i=(n)-1;i>=0;i--)
 #define ll long long
+#define MAIN signed main(void)
+// }}}
 
 int LL_MAX = 9223372036854775807;
 int LL_MIN = -9223372036854775808;
 
 using namespace std;
 
-int gcd(int a, int b);
-int gcd3(int a, int b, int c);
-int lcm(int a, int b);
-void safe_sort(vector<int> *arr);
-void unsafe_sort(vector<int> *arr);
-bool is_prime(int num);
-vector<int> generate_prime(int num);
 // }}}
 
-signed main() {
-
-  return 0;
-}
-
-// {{{
-// ２数の最大公約数
 int gcd(int a, int b) {
+  // ２数の最大公約数 {{{
   int t; while (b != 0) { t = b; b = a % b; a = t; } return a;
-}
-
-// ３数の最大公約数
+} // }}}
 int gcd3(int a, int b, int c) {
+  // ３数の最大公約数 {{{
   int s, t; s = gcd(a, b); t = gcd(c, s); return t;
-}
-
-// 最大公倍数
+} // }}}
 int lcm(int a, int b) {
+  //  最大公約数 {{{
   if (a * b == 0) return 0; return (a * b / gcd(a, b));
-}
-
-/** {{{
- * 安全なソート
- * 使い方
- * vector<int> arr; // データ列を vectorで用意
- * safe_sort(&arr);     // アドレスを渡す
- }}} */
-void safe_sort(vector<int> *arr) {
-  // 逆順にするとき
-  // return stable_sort(arr->begin(), arr->end(), greater<int>());
-  return stable_sort(arr->begin(), arr->end());
-}
-
-/** {{{
- * 高速なソート
- * 使い方
- * vector<int> arr; // データ列を vectorで用意
- * unsafe_sort(&arr);     // アドレスを渡す
- }}} */
-void unsafe_sort(vector<int> *arr) {
-  // 逆順にするとき
-  // return sort(arr->begin(), arr->end(), greater<int>());
-  return sort(arr->begin(), arr->end());
-}
-
-// 素数判定
+} // }}}
+vector<int> in_arr(int size) {
+	// 配列入力 {{{
+	vector<int> arr;
+	REP(i, size) {
+		int data;
+		cin >> data;
+		arr.push_back(data);
+	}
+	return arr;
+} // }}}
+void out_arr(vector<int> arr) {
+  // 配列出力 {{{
+	REP(i, arr.size()) {
+		cout << arr[i] << " ";
+	}
+  cout << endl;
+} // }}}
 bool is_prime(int num) {
+	// 素数判定 {{{
   if (num < 2) return false;
   else if (num == 2) return true;
   else if (num % 2 == 0) return false;
@@ -124,19 +106,23 @@ bool is_prime(int num) {
   double root = sqrt(num);
   for (int i = 3; i <= root; i += 2) { if (num % i == 0) return false; }
   return true;
-}
-
-/** {{{
- *  素数列を生成
- *  使い方
- *  vector<int> arr = generate_prime(n);
- }}} */
-vector<int> generate_prime(int num) {
+} // }}}
+vector<int> gen_prime(int num) {
+  // n以下の素数生成 {{{
   vector<int> arr;
   for (int i = 2; i < num; i++) {
     if (is_prime(i)) arr.push_back(i);
   }
   return arr;
+} // }}}
+
+MAIN {
+  int n;
+  cin >> n;
+  vector<int> arr = gen_prime(n);
+  sort(arr.begin(), arr.end(), greater<int>());
+  out_arr(arr);
+  return 0;
 }
-// }}}
+
 // vim: ft=cpp ts=2 sts=2 sw=2 tw=0
